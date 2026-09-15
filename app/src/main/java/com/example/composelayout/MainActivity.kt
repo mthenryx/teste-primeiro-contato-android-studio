ef743b43ffdd86234978eed25548034f6294cade
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1242373.cfp127aluno.local>
2026-09-15 15:33:54 -0300

    Finalizado essa primeira impressão

--- app/src/main/java/com/example/composelayout/MainActivity.kt
+++ app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelayout.ui.theme.ComposeLayoutTheme
@@ -22,11 +29,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Text("Aula de android")
-                    }
+                    LayoutScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
@@ -34,17 +40,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LayoutScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.background(Color.Gray)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.background(Color.Green)
+                .weight(1f)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text("Texto 1")
+            Text("Texto 2")
+            Text("Texto 3")
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeLayoutTheme {
-        Greeting("Android")
+        Row(
+            modifier = Modifier.background(Color.Red)
+                .weight(1f)
+                .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Texto 4")
+            Text("Texto 5")
+            Text("Texto 6")
+        }
     }
 }
+
